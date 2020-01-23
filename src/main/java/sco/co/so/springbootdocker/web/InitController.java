17d84bda932767cd2208a13507b1b2b3fa6376a8
craig <solksjaer64@hotmail.com>
2020-01-23 14:45:18 +0100

    initial

---
+++ src/main/java/sco/co/so/springbootdocker/web/InitController.java
@@ -0,0 +1,13 @@
+package sco.co.so.springbootdocker.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitController {
+
+    @GetMapping(value="init")
+    public String init() {
+        return "System Up.  Running on " + System.getProperty("os.arch") + " -" + System.getProperty("os.name") + " -" + System.getProperty("os.version");
+    }
+}
---
+++ src/test/java/sco/co/so/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package sco.co.so.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
